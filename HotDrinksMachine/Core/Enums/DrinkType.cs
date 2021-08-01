92b4a56456774635dd5013fdeaa8cf0fd5e3c8ff
gregh <gregholley@hotmail.com>
2021-08-01 22:24:50 +0100

    GH - Functional and UI changes

---
+++ HotDrinksMachine/Core/Enums/DrinkType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HotDrinksMachine.Core.Enums
+{
+    public enum DrinkType
+    {
+        [Description("Not set")]
+        Notset = 0,
+
+        [Description("Lemon Tea")]
+        LemonTea = 1,
+
+        [Description("Coffee")]
+        Coffee = 2,
+
+        [Description("Hot Chocolate")]
+        Chocolate =3,
+
+        [Description("Tea")]
+        Tea = 4,
+    }
+}
